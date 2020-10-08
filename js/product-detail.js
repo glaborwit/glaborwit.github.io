d3a5baea0d23d87a7c56e3c826419a3c5b433c99
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-10-08 13:53:40 -0400

    files updated

--- js/product-detail.js
+++ js/product-detail.js
@@ -1,14 +1,19 @@
 // globals
 let selectedGlaze;
 let selectedQuantity;
+let addToCart;
 
 function selectGlaze(curr){
   // if a glaze is already selected, reset background color (deselect it visually)
   if(selectedGlaze){
-    selectedGlaze.style.backgroundColor="#ffffff";
+    selectedGlaze.style.backgroundColor="#fff";
   }
-  curr.style.backgroundColor="#FBF19A";
+  curr.style.backgroundColor="#eef8ff";
   selectedGlaze = curr;
+
+  if(selectedQuantity && !addToCart){
+    triggerAddToCart();
+  }
 }
 
 function selectQuantity(curr){
@@ -16,6 +21,19 @@ function selectQuantity(curr){
   if(selectedQuantity){
     selectedQuantity.style.backgroundColor="#ffffff";
   }
-  curr.style.backgroundColor="#FBF19A";
+  curr.style.backgroundColor="#eef8ff";
   selectedQuantity = curr;
+
+  if(selectedGlaze && !addToCart){
+    triggerAddToCart();
+  }
+}
+
+function triggerAddToCart(){
+  if (selectedGlaze && selectedQuantity){
+    document.getElementById('addToCart').style.backgroundColor="#FBF19A";
+    document.getElementById('addToCart').style.cursor="pointer";
+
+    addToCart = true;
+  }
 }

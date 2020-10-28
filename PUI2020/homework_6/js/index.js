fcab6405e0cf4168120d2af0d1d69a8c361c5732
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-10-28 10:28:40 -0400

    added img alt tags

--- PUI2020/homework_6/js/index.js
+++ PUI2020/homework_6/js/index.js
@@ -21,27 +21,27 @@ function changeSlide(dot){
     // change to image 1
     if (pic==="img1"){
         document.getElementById("slideshow-pic").src = "assets/bun-buns/originalcinnamon.jpg";
-        document.getElementById("bun-bun-name").innerHTML="<a href='original-cinnamon.html'>Original Cinnamon Bun Bun</a>";
+        document.getElementById("bun-bun-name").innerHTML="<a alt='photo of original cinnamon bun bun' href='original-cinnamon.html'>Original Cinnamon Bun Bun</a>";
         document.getElementById("tagline").innerHTML="Go back to the basics with our best-selling bun bun!";
 
     }
     // change to image 2
     if (pic==="img2"){
         document.getElementById("slideshow-pic").src = "assets/bun-buns/walnut.jpg";
-        document.getElementById("bun-bun-name").innerHTML="<a href='walnut.html'>Walnut Bun Bun</a>";
+        document.getElementById("bun-bun-name").innerHTML="<a alt='photo of walnut bun bun' href='walnut.html'>Walnut Bun Bun</a>";
         document.getElementById("tagline").innerHTML="Another popular flavor, walnut, gives you a crunch in every bite.";
 
     }
     // change to image 3
     if (pic==="img3"){
         document.getElementById("slideshow-pic").src = "assets/bun-buns/gf.jpg";
-        document.getElementById("bun-bun-name").innerHTML="<a href='glutenfree-cinnamon.html'>Gluten-Free Cinnamon Bun Bun</a>";
+        document.getElementById("bun-bun-name").innerHTML="<a alt='photo of gluten-free cinnamon bun bun' href='glutenfree-cinnamon.html'>Gluten-Free Cinnamon Bun Bun</a>";
         document.getElementById("tagline").innerHTML="Now everyone can enjoy our buns with our gluten-free rolls!";
     }
     // change to image 4
     if (pic==="img4"){
         document.getElementById("slideshow-pic").src = "assets/bun-buns/caramelpecan.jpg";
-        document.getElementById("bun-bun-name").innerHTML="<a href='caramelpecan.html'>Caramel Pecan Bun Bun</a>";
+        document.getElementById("bun-bun-name").innerHTML="<a alt='photo of caramel pecan bun bun' href='caramelpecan.html'>Caramel Pecan Bun Bun</a>";
         document.getElementById("tagline").innerHTML="Both sweet and nutty, you'll be craving this caramel treat.";
     }
 }

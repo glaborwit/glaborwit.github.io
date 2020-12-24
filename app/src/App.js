c7a29f6088b61f721d4e6fee8e591f067cf50bfb
Gabrielle LaBorwit <gabriellelaborwit@gmail.com>
2020-12-23 20:06:47 -0500

    made taglines lowercase on index page

--- app/src/App.js
+++ app/src/App.js
@@ -7,7 +7,6 @@ import './css/App.css';
 import './css/Main.css';
 
 // Images
-// import PurpleSolid from './assets/images/solid-purple.png';
 import TimeWarp from './assets/images/time-warp/timewarpclock.png';
 import DashDesign from './assets/images/dashupdate.gif';
 import BreakingtheIceCover from './assets/images/UCRE/BreakingtheIceCover.png';
@@ -19,9 +18,9 @@ function App() {
         <h1 className="name text-center">GABRIELLE LABORWIT</h1>
         <h2 className="purple text-center">UX Researcher</h2>
         <div className="big-margin-top row justify-content-center">
-          <Infocard title="Breaking the Ice" affiliation="Carnegie Mellon University" tagline="A Virtual Theater Community Experience" pic={BreakingtheIceCover} link="breaking-the-ice"/>
+          <Infocard title="Breaking the Ice" affiliation="Carnegie Mellon University" tagline="A virtual theater community experience" pic={BreakingtheIceCover} link="breaking-the-ice"/>
 
-          <Infocard title="Time Warp in AR" affiliation="Ludwig-Maximilian University of Munich, Human-Centered Ubiquitous Media Lab" tagline="Slowing Down Time in Augmented Reality" pic={TimeWarp} link="time-warp"/>
+          <Infocard title="Time Warp in AR" affiliation="Ludwig-Maximilian University of Munich, Human-Centered Ubiquitous Media Lab" tagline="Slowing down time in augmented reality" pic={TimeWarp} link="time-warp"/>
         </div>
 
         <div className="big-margin-top row justify-content-center">
